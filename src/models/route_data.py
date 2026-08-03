8b856f31954a50d311c681355a5286229106746b
ztt1-1 <zacharytrust1@gmail.com>
2026-08-03 12:50:53 -0700

    Very tiny changes, just some edits and additions to routes data and service, nothing works for either still though.

---
+++ src/models/route_data.py
@@ -0,0 +1,3 @@
+from src.services.weather_service import Route
+from src.services.api import GOOGLE_MAPS_API_KEY #imports the api key from src.services.api
+
--- src/services/routes_service.py
+++ src/services/routes_service.py
@@ -7,9 +7,8 @@ class Route: #creates Route class, used to find a route between lat and long pts
         self.url = "https://routes.googleapis.com/directions/v2:computeRoutes" #the link to the api
 
     def get_route(self, headers, data): #the definition used to get the route, takes lat/long data and the headers and the transportation type from src.services.route_data from user input
-        headers =
-        data = {"key": self.api_key, "q": location} #the parameters for the below line of code to be able to get the info from the api
-        response = requests.get(self.url, params=parameters) #gets the api data with those parameters (above) and the url
+        data = {"key": self.api_key} #the parameters for the below line of code to be able to get the info from the api
+        response = requests.get(self.url, headers=headers, json=data) #gets the api data with those parameters (above) and the url
 
         if response.status_code != 200: #if the request fails, do not crash code, just let user know the weather is not available and also show the error code
             print(f"Weather not available, error code: {response.status_code}")
@@ -17,3 +16,5 @@ class Route: #creates Route class, used to find a route between lat and long pts
 
         weather_data = response.json() #variable weather_data stores the stripped JSON text of the api data, 'tis only a bit different from the raw text but whatever it's easier to use
         return weather_data #sends the weather data to src.services.weather_service so that src.services.weather_service can print the data found through this class
+
+    #note to future zach, FINISH THIS!!!!! figure out data and response stuff (not finished), thx!
